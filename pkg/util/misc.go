8520415399e41fc8bec13b14542f4db00ed5039f
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-10-22 04:08:52 +0000

    2022.10.22-04:08

--- pkg/util/misc.go
+++ pkg/util/misc.go
@@ -32,6 +32,8 @@ import (
 // ref: // https://pkg.go.dev/golang.org/x/mod/semver#Major
 // clang may not work well because version output results may differ depending
 // on the platform.
+//
+// DOCKER-TESTED: [centos:centos7, ubuntu:22.04, fedora:37]
 func GetClangVersion() (string, error) {
 	clangPath := which.Which("clang")
 	out, err := LocalExecutef("%s --version", clangPath)
@@ -39,13 +41,20 @@ func GetClangVersion() (string, error) {
 		return "", err
 	}
 
-	// [EXAMPLE:centos]
+	// [EXAMPLE:centos 7]
 	// $ clang --version
 	// clang version 3.4.2 (tags/RELEASE_34/dot2-final)
 	// Target: x86_64-redhat-linux-gnu
 	// Thread model: posix
 	//
-	// [EXAMPLE:ubuntu]
+	// [EXAMPLE:fedora 37]
+	// $ clang --version
+	// clang version 15.0.0 (Fedora 15.0.0-5.fc38)
+	// Target: x86_64-redhat-linux-gnu
+	// Thread model: posix
+	// InstalledDir: /usr/bin
+	//
+	// [EXAMPLE:ubuntu 22.04]
 	// $ clang --version
 	// Ubuntu clang version 12.0.1-19ubuntu3
 	// Target: x86_64-pc-linux-gnu
@@ -77,6 +86,24 @@ func GetKernelVersion() (string, error) {
 	return "v100.0.0", nil
 }
 
-func GetIproute2Version() (string, error) {
-	return "v100.0.0", nil
+func GetIproute2Version() (string, string, error) {
+	iproute2Path := which.Which("ip")
+	out, err := LocalExecutef("%s -V", iproute2Path)
+	if err != nil {
+		return "", "", err
+	}
+
+	// [EXAMPLE:centos 7]
+	// $ ip -V
+	//
+	// [EXAMPLE:ubuntu 20.04]
+	// $ ip -V
+	//
+	// [EXAMPLE: fedora 37]
+	// $ ip -V
+	// ip utility, iproute2-6.0.0, libbpf 0.8.0
+
+	println(out)
+
+	return "v100.0.0", "v100.0.0", nil
 }
